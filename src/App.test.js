62aafe96b15ca4605fec1ea3a0e5205e3dced687
MMcClure11 <mcclme02@luther.edu>
2021-01-27 19:11:52 -0600

    Add first red-green test

--- src/App.test.js
+++ src/App.test.js
@@ -6,9 +6,11 @@ import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test('renders without error', () => {
-
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find('[data-test="component-app"]');
+  expect(appComponent.length).toBe(1);
 });
-test('rnders button', () => {
+test('renders button', () => {
 
 });
 test('renders counter display', () => {

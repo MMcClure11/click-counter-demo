474e6ec16d3046c616a16ff49a258a635ed2d6fe
MMcClure11 <mcclme02@luther.edu>
2021-01-27 19:03:12 -0600

    Add tests with 5 passing tests, WIP

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,18 @@ import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 //set up enzyme's react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+test('renders without error', () => {
+
+});
+test('rnders button', () => {
+
+});
+test('renders counter display', () => {
+
+});
+test('counter starts at 0', () => {
+
+});
+test('clicking on the button increments counter display', () => {
+
+});
